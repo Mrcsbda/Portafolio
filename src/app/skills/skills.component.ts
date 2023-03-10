54e4b2d43c4b4b0669b0293c987f9a1686ebcd20
Mrcsbda <macbedoya@gmail.com>
2023-03-10 14:26:51 -0500

    skills mobile

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent implements OnInit {
+export class SkillsComponent {
 
-  constructor() { }
+  skillInformation: SkillInformation[] = [
+    {
+      image: '../../assets/skills/HTML.svg',
+      alt: 'HTML5',
+      p: 'HTML 5'
+    },
+    {
+      image: '../../assets/skills/CSS.svg',
+      alt: 'CSS3',
+      p: 'CSS 3'
+    },
+    {
+      image: '../../assets/skills/JavaScript.svg',
+      alt: 'JavaScript',
+      p: 'JavaScript'
+    },
+    {
+      image: '../../assets/skills/TypeScript.svg',
+      alt: 'TypeScript',
+      p: 'TypeScript'
+    },
+    {
+      image: '../../assets/skills/Angular.svg',
+      alt: 'Angular',
+      p: 'Angular'
+    },
+    {
+      image: '../../assets/skills/SASS.svg',
+      alt: 'Sass',
+      p: 'Sass'
+    },
+    {
+      image: '../../assets/skills/Git.svg',
+      alt: 'Git',
+      p: 'Git'
+    },
+    {
+      image: '../../assets/skills/GitHub.svg',
+      alt: 'Github',
+      p: 'GitHub'
+    },
+    {
+      image: '../../assets/skills/Responsive.svg',
+      alt: 'Responsive Websites',
+      p: 'Responsive Websites'
+    },
+    {
+      image: '../../assets/skills/Illustrator.svg',
+      alt: 'Illustrator',
+      p: 'Illustrator'
+    }
+  ]
 
-  ngOnInit(): void {
-  }
+}
 
+interface SkillInformation {
+  image: string;
+  alt: string;
+  p: string;
 }
