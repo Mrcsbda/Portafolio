6547c19d345be3794feb3265571fa5d3fcde24aa
Mrcsbda <macbedoya@gmail.com>
2023-03-13 14:53:02 -0500

    contact mobile

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,33 +8,34 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
 
   projects: ProjectInformation[] = [
+    {
+      title: 'Encriptador',
+      text: 'Aplicación con la cual puedes encriptar y desencriptar un texto; el texto que desees desencriptar debe haber sido encriptado anteriormente en la aplicación. ',
+      class: 'project p1',
+      imgProject: '../../assets/projects/Encriptador.png',
+      demo: 'https://mrcsbda.github.io/Encriptador/',
+      codigo: 'https://github.com/Mrcsbda/Encriptador',
+      imgTecnologias: '../../assets/projects/technologiesCP.svg',
+    },
   {
     title: 'Calculator App',
     text: 'Aplicación con la cual puedes realizar las operaciones básicas talescomo sumas, restas, multiplicaciones y divisiones.',
-    class: 'project p1',
+    class: 'project p2',
     imgProject: '../../assets/projects/Calculadora.png',
     demo: 'https://calculatorapp-lx0wzdk0f-mrcsbda.vercel.app/',
     codigo: 'https://github.com/Mrcsbda/Calculator-App',
-    imgTecnologias: '../../assets/projects/technologiesColorPurple.svg',
+    imgTecnologias: '../../assets/projects/technologiesCWO2.svg',
   },
     {
     title: 'Advices generator',
     text: 'Aplicación que trae anuncios aleatorios y el número del anuncio correspondiente.',
-    class: 'project p2',
+    class: 'project p1',
     imgProject: '../../assets/projects/Anuncios.png',
     demo: 'https://advice-generator-qrzn5ogxm-mrcsbda.vercel.app/',
     codigo: 'https://github.com/Mrcsbda/Advice-generator-App',
-    imgTecnologias: '../../assets/projects/technologiesColorWhite.svg',
-  },
-  {
-    title: 'Encriptador',
-    text: 'Aplicación con la cual puedes encriptar y desencriptar un texto; el texto que desees desencriptar debe haber sido encriptado anteriormente en la aplicación. ',
-    class: 'project p1',
-    imgProject: '../../assets/projects/Encriptador.png',
-    demo: 'https://mrcsbda.github.io/Encriptador/',
-    codigo: 'https://github.com/Mrcsbda/Encriptador',
-    imgTecnologias: '../../assets/projects/technologiesColorPurple.svg',
-  },
+    imgTecnologias: '../../assets/projects/technologiesCPO2.svg',
+  }
+  
   ]
 
 }
